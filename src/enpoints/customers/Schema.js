dc5bc35706f842474a7b11103a71dafc2bdf0bd1
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-03-21 08:12:15 +0100

    privacy and compliance

--- src/enpoints/customers/Schema.js
+++ src/enpoints/customers/Schema.js
@@ -5,8 +5,13 @@ const { Schema, model } = mongoose;
 
 const customerSchema = new Schema({
     name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    userName: { type: String, required: true },
+    password: { type: String, required: true },
     email: { type: String, required: true },
     seller: [{ type: Schema.Types.ObjectId, ref: "Sales" }],
+    isPrivacy: { type: Boolean, required: true },
+    isCompliance: { type: Boolean, required: true },
     role: {
         type: String,
         enum: ["User", "Salesforce", "Admin"],
