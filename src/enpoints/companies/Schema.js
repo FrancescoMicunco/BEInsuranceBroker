f2e2d7d074328c6a9085d1e11115b309ad3b5f97
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-03-27 11:32:14 +0200

    company schema

--- src/enpoints/companies/Schema.js
+++ src/enpoints/companies/Schema.js
@@ -5,6 +5,24 @@ const { Schema, model } = mongoose;
 const companySchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
+    phone: { type: String, required: true },
+    products: [{
+        productNmber: { type: String, required: true },
+        productName: { type: String, required: true },
+        area: {
+            type: String,
+            required: true,
+            enum: ["Life", "Healt", "Professional"],
+        },
+        minAge: { type: String, required: true },
+        maxAge: { type: String, required: true },
+        minDuration: { type: String, required: true },
+        marriageStatus: {
+            type: String,
+            required: true,
+            enum: ["Married", "Divorced", "Separate", "Celibate/Maiden"],
+        },
+    }, ],
 }, { timestamp: true });
 
 export default model("Companies", companySchema);
